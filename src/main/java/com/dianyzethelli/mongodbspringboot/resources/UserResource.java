a306c43b802b54cda6d880da8c270d536ac8c971
Thiago Azevedo <thiago7azevedo@gmail.com>
2019-10-17 15:03:53 -0300

    DTO to return useres

--- src/main/java/com/dianyzethelli/mongodbspringboot/resources/UserResource.java
+++ src/main/java/com/dianyzethelli/mongodbspringboot/resources/UserResource.java
@@ -1,8 +1,7 @@
 package com.dianyzethelli.mongodbspringboot.resources;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dianyzethelli.mongodbspringboot.domain.User;
+import com.dianyzethelli.mongodbspringboot.dto.UserDTO;
 import com.dianyzethelli.mongodbspringboot.services.UserService;
 
 @RestController
@@ -21,9 +21,16 @@ public class UserResource {
 	private UserService service;
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public ResponseEntity <List<User>> findAll(){ // mais robusto utilizar ResponseEntity passando a lista dentro <>
+	public ResponseEntity <List<UserDTO>> findAll(){ // mais robusto utilizar ResponseEntity passando a lista dentro <>
 		List<User> list = service.findALL();
-		return ResponseEntity.ok().body(list); // retorna esse metodo se esta OK e no corpo (body) a lista.
+		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+//		expressão lamda para converter cada objeto da lista de User para UserDTO.
+//		list.stream(transforma na coleção compativel com lambda).
+//		.map (cada objeto x da lista original).
+//		-> (aponta do objeto original para criar um novo objeto new UserDTO, passando x como argumento).
+//		.collect(coleta toda expressão e volta ou converte novamente para lista). Passa como cargumento a operação Collectros voltando para lista.
+		
+		return ResponseEntity.ok().body(listDto); // retorna esse metodo se esta OK e no corpo (body) a lista DTO.
 	}
 
 }
