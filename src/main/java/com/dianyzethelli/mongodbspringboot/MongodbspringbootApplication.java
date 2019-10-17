340bd7ff2106322c57bc3aed416b7bc745e0621e
Thiago Azevedo <thiago7azevedo@gmail.com>
2019-10-17 13:27:36 -0300

    Project created

---
+++ src/main/java/com/dianyzethelli/mongodbspringboot/MongodbspringbootApplication.java
@@ -0,0 +1,13 @@
+package com.dianyzethelli.mongodbspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongodbspringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongodbspringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dianyzethelli/mongodbspringboot/MongodbspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dianyzethelli.mongodbspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongodbspringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
