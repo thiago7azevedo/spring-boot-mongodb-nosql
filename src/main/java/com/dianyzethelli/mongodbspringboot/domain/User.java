a34e56db470dc94ae112b037e6b4591c1dac9e68
Thiago Azevedo <thiago7azevedo@gmail.com>
2019-10-18 18:03:20 -0300

    DBRef and lazy true - Endpoint return posts

--- src/main/java/com/dianyzethelli/mongodbspringboot/domain/User.java
+++ src/main/java/com/dianyzethelli/mongodbspringboot/domain/User.java
@@ -1,8 +1,11 @@
 package com.dianyzethelli.mongodbspringboot.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 
@@ -14,7 +17,10 @@ public class User implements Serializable {
 	private String id;
 	private String name;
 	private String email;
-	
+		
+	@DBRef(lazy = true)// lazy true para evitar que carregue uma lisa de posts de cada usuario caso queira somente lista de usuario
+	private List<Post> posts = new ArrayList<>();
+	// DBRef é apenas reerencia de uma coleção que está em outro objeto. 
 	public User() {	
 	}
 
@@ -49,6 +55,14 @@ public class User implements Serializable {
 		this.email = email;
 	}
 
+	public List<Post> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -72,5 +86,7 @@ public class User implements Serializable {
 		} else if (!id.equals(other.id))
 			return false;
 		return true;
-	}	
+	}
+
+	
 }
